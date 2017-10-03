8fe1bced092d5893da856d7b3bcc0ea08977082f
teshi24 <nadja.stadi@gmail.com>
2017-10-03 16:01:24 +0200

    AddItemViews mit ersten Überprüfungen, changes der Items und Testaufruf der im program übernommen werden kann.

--- src/starbucks_fx/AddItem.java
+++ src/starbucks_fx/AddItem.java
@@ -1,6 +1,5 @@
 package starbucks_fx;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -8,111 +7,157 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-import starbucks.Menu;
+import javafx.scene.layout.VBox;
 import starbucks.MenuItemFactory;
 
-public class AddItem extends Application {
+public class AddItem {
     // all possible fields with item information
-    Label message, nameL, priceL, ingredientsL, optionalL;
-    TextField name, price, ingredients, optional;
+    private Label message, nameL, priceL, ingredientsL, optionalL;
+    private TextField name, price, ingredients, optional;
 
-    // required
-    Button addItem;
-    GridPane form;
-    MenuItemFactory factory;
+    private FlowPane choice;
+    private VBox bigPicture;
+    private Label choose;
 
-    public static void main(String[] args) {
-        launch(args);
+    private GridPane form;
+    private Button addItem;
+    private int category = -1;
+    /*
+    double priceD = Double.parseDouble(price.getText());
+    boolean dietB;
+
+    if(optional.getText().equals("hot")){
+        dietB = true;
+    }else if(optional.getText().equals("cold")){
+        dietB = false;
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        factory = MenuItemFactory.getInstance();
+    //addItem.setOnAction((ActionEvent e) -> add(name.getText(),priceD,ingredients.getText(),dietB));
 
-        primaryStage.setScene(callAddItem(0));
-        primaryStage.show();
+    //Scene addFood = new Scene();
+    */
+    public Scene showAddItem(){
+        bigPicture = new VBox();
+        bigPicture.setPadding(new Insets(10,10,10,10));
+        bigPicture.setSpacing(10);
 
-        /*
-        double priceD = Double.parseDouble(price.getText());
-        boolean dietB;
+        choice = new FlowPane();
+        choice.setPadding(new Insets(0,10,0,10));
+        choice.setHgap(10);
+        choice.setVgap(15);
 
-        if(optional.getText().equals("hot")){
-            dietB = true;
-        }else if(optional.getText().equals("cold")){
-            dietB = false;
+        choose = new Label();
+
+        Button addCoffee = new Button("coffee");
+        Button addFood = new Button("food");
+        Button addBeverage = new Button("beverage");
+        Button addExtra = new Button("extra");
+
+        addCoffee.setMinSize(50,40);
+        addFood.setMinSize(50,40);
+        addBeverage.setMinSize(50,40);
+        addExtra.setMinSize(50,40);
+
+        addCoffee.setOnAction((ActionEvent e) -> {category=0;showForm(category);});
+        addFood.setOnAction((ActionEvent e) -> {category=1;showForm(category);});
+        addBeverage.setOnAction((ActionEvent e) -> {category=2;showForm(category);});
+        addExtra.setOnAction((ActionEvent e) -> {category=3;showForm(category);});
+
+        choice.getChildren().addAll(addCoffee,addFood,addBeverage,addExtra);
+
+        if(form != null){
+            choose.setText("Change the item type if needed.");
+            bigPicture.getChildren().addAll(choose,choice,form);
+        }else{
+            choose.setText("Choose the item you want to add");
+            bigPicture.getChildren().addAll(choose,choice);
         }
-
-        //addItem.setOnAction((ActionEvent e) -> add(name.getText(),priceD,ingredients.getText(),dietB));
-
-        //Scene addFood = new Scene();
-        */
+        return new Scene(bigPicture,500,500);
     }
 
-    public Scene callAddItem(int category){
-        //initialize standard information
-        message = new Label();
-        nameL = new Label("name:");
-        priceL = new Label("price:");
-        name = new TextField();
-        price = new TextField();
 
-        form = new GridPane();
-        form.setAlignment(Pos.CENTER);
-        form.setHgap(10);
-        form.setVgap(5);
-        form.setPadding(new Insets(10,10,10,10));
+    public GridPane callAddItem(int category){
+        if(category >= 0) {
+            //initialize standard information
+            message = new Label();
+            nameL = new Label("name:");
+            priceL = new Label("price:");
+            name = new TextField();
+            price = new TextField();
 
-        addItem = new Button("add Item");
-        addItem.setMinSize(50,40);
+            form = new GridPane();
+            form.setAlignment(Pos.CENTER);
+            form.setHgap(10);
+            form.setVgap(5);
+            form.setPadding(new Insets(10, 10, 10, 10));
 
-        addItem.setOnAction((ActionEvent e) -> {
-            boolean ok = false;
-            String nam;
-            double pri;
-            String ing;
-            boolean opt;
-            String mes = "";
-            // check name and price
-            if(ingredients != null){
-                ing = ingredients.getText();
-            }else{
+            addItem = new Button("add Item");
+            addItem.setMinSize(50, 40);
 
+            addItem.setOnAction((ActionEvent e) -> {
+                boolean ok = false;
+                String nam = "";
+                double pri = 0;
+                String ing;
+                String opt;
+                String mes = "";
+                // check inserted values
+                if (name != null && !name.getText().equals("")) {
+                    nam = name.getText();
+                } else {
+                    mes += "Please enter a name for your product." + System.lineSeparator();
+                    ok = false;
+                }
+                try {
+                    pri = Double.parseDouble(price.getText());
+                } catch (Exception ex) {
+                    mes += "Please enter a valid price. (Format: 9.99)";
+                    ok = false;
+                }
+                if (ingredients != null) {
+                    ing = ingredients.getText();
+                } else {
+                    ing = null;
+                }
+                if (optional != null) {
+                    opt = optional.getText();
+                } else {
+                    opt = null;
+                }
+                //send values to Factory
+                if (ok) {
+                    MenuItemFactory factory = MenuItemFactory.getInstance();
+                    factory.create(nam, pri, ing, opt);
+                } else {
+                    message.setText(mes);
+                }
+            });
+
+            form.add(message, 0, 0, 2, 1);
+            form.add(nameL, 0, 1);
+            form.add(name, 1, 1);
+            form.add(priceL, 0, 2);
+            form.add(price, 1, 2);
+
+            switch (category) {
+                case 0:
+                    addCoffee();
+                    break;
+                case 1:
+                    addFood();
+                    break;
+                case 2:
+                    addBeverage();
+                    break;
+                case 3:
+                    addExtra();
+                    break;
             }
-            if(optional != null){
-
-            }else{
-
-            }
-
-            if(ok){
-                //MenuItemFactory factory = MenuItemFactory.getInstance();
-                //(starbucks.MenuItemFactory)factory.add(nam,pri,ing,opt);
-            }else{
-                message.setText(mes);
-            }
-        });
-
-        form.add(message,0,0,2,1);
-        form.add(nameL,0,1);
-        form.add(name,1,1);
-        form.add(priceL,0,2);
-        form.add(price,1,2);
-
-        switch(category){
-            case 0: addCoffee();
-                    break;
-            case 1: addFood();
-                    break;
-            case 2: addBeverage();
-                    break;
-            case 3: addExtra();
-                    break;
-            default: return null;
+            return form;
         }
-
-        return new Scene(form,300,300);
+        return null;
     }
 
     private void addCoffee(){
@@ -159,6 +204,14 @@ public class AddItem extends Application {
         message.setText("Add some " + item + " to your menu.");
     }
 
+    private void showForm(int category){
+        if(form != null){
+            bigPicture.getChildren().remove(2);
+        }
+        callAddItem(category);
+        choose.setText("Change the item type if needed.");
+        bigPicture.getChildren().add(form);
+    }
     /*
     public void add(String name, double price, String ingredients, boolean optional = null){
         //
