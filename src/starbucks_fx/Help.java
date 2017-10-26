56bf702bc21251943975aeef3fd937794a3565aa
teshi24 <nadja.stadi@gmail.com>
2017-10-26 16:56:26 +0200

    doku und help

--- src/starbucks_fx/Help.java
+++ src/starbucks_fx/Help.java
@@ -4,11 +4,9 @@
 
 package starbucks_fx;
 
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
@@ -18,36 +16,47 @@ import javafx.stage.Stage;
 
 public class Help {
 
+    private Label showTitle, addTitle, modifyTitle, deleteTitle, wrongCharTitle, show, add, modify, delete, wrongChar;
+
     public Help(Stage ownerStage){
         Stage helpStage = new Stage();
         helpStage.getIcons().add(new Image("resources/images/questionMark.jpg"));
         helpStage.setTitle("Help");
-        helpStage.setScene(new Scene(setPane(helpStage)));
+        helpStage.setScene(new Scene(setPane()));
         helpStage.initOwner(ownerStage);
         helpStage.setResizable(false);
         helpStage.show();
     }
 
     /**
-     * @param errorStage
      * @return
      */
-    private static VBox setPane(Stage errorStage){
+    private VBox setPane(){
         VBox pane = new VBox();
         pane.setAlignment(Pos.TOP_LEFT);
         pane.setPadding(new Insets(10,10,10,10));
         pane.setSpacing(10);
         pane.getStylesheets().add("resources/css/style.css");
 
-        Label msg = new Label("This is a Text.");
-        Button ok = new Button("OK");
-        ok.setPrefSize(100, 30);
+        showTitle       = new Label("Client view");
+        show            = new Label("To show the menu to the clients, simply press the 'Home'-Button." + System.lineSeparator()
+                                    +   "If there aren't any items shown, add some items to the menu.");
+        addTitle        = new Label("Add an item");
+        add             = new Label("To add a new menu item, press the menu button 'Add'." + System.lineSeparator()
+                                    +   "Choose the type of item you want to add." + System.lineSeparator()
+                                    +   "Enter the required infos");
+        modifyTitle     = new Label("Modify an item");
+        modify          = new Label("This is a Text.");
+        deleteTitle     = new Label("Delete an item");
+        delete          = new Label("This is a Text.");
+        wrongCharTitle  = new Label("Delete an item");
+        wrongChar       = new Label("This is a Text.");
 
-        ok.setOnAction((ActionEvent e) -> {
-            errorStage.close();
-        });
-
-        pane.getChildren().addAll(msg, ok);
+        pane.getChildren().addAll(showTitle     ,show
+                                 ,addTitle      ,add
+                                 ,modifyTitle   ,modify
+                                 ,deleteTitle   ,delete
+                                 ,wrongCharTitle,wrongChar);
         return pane;
     }
 
